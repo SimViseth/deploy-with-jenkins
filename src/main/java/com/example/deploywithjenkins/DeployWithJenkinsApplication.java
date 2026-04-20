78e6cbd3d343ae266cd3cc6cad08ab1b55d36db7
viseth <simviseth509@gmail.com>
2026-04-20 21:14:36 +0700

    first commit

---
+++ src/main/java/com/example/deploywithjenkins/DeployWithJenkinsApplication.java
@@ -0,0 +1,20 @@
+package com.example.deploywithjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DeployWithJenkinsApplication {
+
+    @GetMapping
+    public String message() {
+        return "Hello !";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployWithJenkinsApplication.class, args);
+    }
+}
